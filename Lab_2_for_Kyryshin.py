f4510b7fd920162fa106776607b7175b594bb06b
Irina Stepanova <110852608+Irissha@users.noreply.github.com>
2022-12-04 17:57:05 +0500

    Update Lab_2_for_Kyryshin.py

--- Lab_2_for_Kyryshin.py
+++ Lab_2_for_Kyryshin.py
@@ -21,3 +21,16 @@ myplayears.append(player2)
 
 json.dump(myplayers, myfile)
 myfile.close()
+
+#----------- LOAD by JSON ----------
+
+myfile = open(filename, mode='r', encoding='Latin-1')
+json_data = json.load(myfile)
+
+for user in json_data:
+  print('Player Name is ' + str(user['PlayerName']))
+  print('Player Score is ' + str(user['Score']))
+  print('Player Award №1: ' + str(user['awards'][0]))
+  print('Player Award №2: ' + str(user['awards'][1]))
+  print('Player Award №3: ' + str(user['awards'][2]))
+  print("-----------------------------\n\n")
