9bb51037d7ec2250760a344c34be73082574443f
Irina Stepanova <110852608+Irissha@users.noreply.github.com>
2022-12-04 15:41:15 +0500

    Update Lab_2_for_Kyryshin.py

--- Lab_2_for_Kyryshin.py
+++ Lab_2_for_Kyryshin.py
@@ -1,6 +1,1 @@
-import numpy as np
-print(np.mean([i for i in range(999999+1)]))
-print(np.sum([i for i in range(999999+1)]))
-x = np.random.normal(size=1000000)
-print(np.median(x))
-print(np.prod(x))
+import json
