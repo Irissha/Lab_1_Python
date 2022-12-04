acf19c0d4c6a70b5ee370b7fa8567e0b263fec75
Irina Stepanova <110852608+Irissha@users.noreply.github.com>
2022-12-04 17:37:17 +0500

    Update Lab_2_for_Kyryshin.py

--- Lab_2_for_Kyryshin.py
+++ Lab_2_for_Kyryshin.py
@@ -1,1 +1,23 @@
 import json
+filename = "user_settings.txt"
+myfile = open(filename, mode = 'w', encoding = 'Latin-1')
+
+player1 {
+  'PlayerName': "Donald Trump",
+  'Score': 345,
+  'awards': ["OR", "NV", "NY"]
+}
+
+player2 {
+  'PlayerName': "Clinton Hillary",
+  'Score': 346,
+  'awards': ["WI", "TX", "MI"]
+}
+myplayears = []
+myplayears.append(player1)
+myplayears.append(player2)
+
+# ---------- SAVE by JSON -----------
+
+json.dump(myplayers, myfile)
+myfile.close()
