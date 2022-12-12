8a6662427eead8f210ceb558e56fb99d3a5c43e0
Irina Stepanova <110852608+Irissha@users.noreply.github.com>
2022-12-12 20:25:29 +0500

    Update Lab_3_for_pass.py

--- Lab_3_for_pass.py
+++ Lab_3_for_pass.py
@@ -2,3 +2,7 @@ from pyDatalog import pyDatalog
 
 import random
 pyDatalog.create_terms('X, Z, Y, Sm, div, Average, y, SumRand')
+(y[X] == sum_(Y, for_each=Y)) <= ((Y._in(range_(X+1))))
+print(y[0] == Sm)
+(div[X, Y] == Z) <= (X // Y == Z)
+print(div[y[0], 0] == Average)
