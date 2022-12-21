be67d0e81cf2c599d2ca5d7921788e66b9fffd0d
Irisha <irina.stepanova16@mail.ru>
2022-12-21 15:40:36 +0500

    314 women_passengers

--- Lab4/lab4_map.py
+++ Lab4/lab4_map.py
@@ -6,17 +6,5 @@ with open("titanic.csv","r") as file:
 	reader = csv.reader(file, delimiter=",")
 	dataset = [x for x in reader]
 keys = list(keys.split(","))
-
-def to_dictionary(keys, values):
-	dictionary = {}
-	for i in range(len(keys)):
-		dictionary[keys[i]] = values[i]
-	return dictionary
-
-dataset = list(map(lambda x: to_dictionary(keys, x), dataset))
-
-age = list(map(lambda x: int(x['Amount']), dataset))
-sum_of_age = reduce(lambda x, y: x + y, age)
-average_age = round(sum_of_age / len(age), 2)
-
-print("Sum of age:", sum_of_age,"\nCount of Assistance:",len(age),"\nAverage Assistance:", average_age)
+women_passengers = list(map(lambda x: x[4] == 'female', dataset))
+print(women_passengers.count(True))
